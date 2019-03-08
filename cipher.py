b92a85f2ae8aa10b98d2e92c78cdc9be21ce0f75
kmrakash <47351616+kmrakash@users.noreply.github.com>
2019-03-08 15:08:21 +0530

    Keep your text Secret
    if you want to keep your letters secret use the code to convert in diffrent manner like "a" converted into "V" , "b" converted int "W" etc.

---
+++ cipher.py
@@ -0,0 +1,24 @@
+import string
+dict = {}
+data = ""
+for i in range(len(string.ascii_letters)):
+    dict[string.ascii_letters[i]] = string.ascii_letters[i - 5]
+print(dict) 
+X = open("test.txt",'r',encoding = 'utf-8')  
+print(X.read()) 
+print("After encoding the letter in different manner ")
+file = open("encoded.txt",'w',encoding = 'utf-8')
+with open("test.txt") as f:
+    while True:
+        c = f.read(1)
+        if not c:
+            print("ENd OF THE FILE")
+            break
+        if c in dict:
+            data = dict[c]
+        else:
+            data = c
+        file.write(data)
+file.close()            
+a = open("encoded.txt",'r')
+print(a.read())      
